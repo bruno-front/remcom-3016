e99fa9bda1bfda2d00102651bd2bbd15e9d27730
Kirill Demidov <kademidov@gmail.com>
2023-08-31 15:12:07 +0300

    Что тут: - Полный адаптив главной

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,27 @@
 console.log('script work!');
+
+
+let age;
+
+console.log(age);
+
+age = 39;
+
+console.log(age);
+
+const Pi = 3.14;
+
+console.log(Pi);
+
+
+
+let petName = 'Kesha';
+let birdName = 'Karl';
+
+console.log(petName);
+
+let a = '4';
+let b = '3';
+
+console.log(a * b);
+console.log(petName * birdName);
