f737933f4858a5cb69cec1c5ddab0218af6fc0bc
Kirill Demidov <kademidov@gmail.com>
2023-09-09 22:39:34 +0300

    Что тут: - Открытие закрытие мобильного меню - Табы в контактах - Частично разметка внутренних страниц

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,15 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // Покрасили все .services-item в синий цвет
-  $('.services-item').css({'color': 'blue'});
+  // let isOpen = false;
 
-});
+  $('.header-burger').on('click', function () {
 
-// Пример добавления слушатели по клику на бургер на чистом jS
-let burger = document.querySelector('.header-burger');
+    // if (isOpen) {
+    //   функция колбек, которая удалит атрибут style,
+    //   когда завершится анимация закрытия меню
+    //                               |
+    //   $('.menu').slideUp(400, function () {
+    //     $('.menu').removeAttr('style');
+    //   })
+    //   isOpen = false;
+    // } else {
+    //   $('.menu').slideDown();
+    //   isOpen = true;
+    // }
 
-burger.addEventListener('click', function() {
-  // При нажатии на кнопку в консоль будет выводиться
-  // строка 'click'
-  console.log('click');
+    $('.menu').slideToggle();
+
+  });
+
+
+  // Табы в контактах!
+  $('.js-tab-link').on('click', function (event) {
+    event.preventDefault();
+
+    $('.js-tab-link').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.js-tab-link');
+
+    $('.js-contacts-info').removeClass('visible');
+    $('.js-contacts-info').eq(index).addClass('visible');
+  });
+
+
+  // Это не трож!!!!
 });
